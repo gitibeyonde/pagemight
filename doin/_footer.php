32f39b8fd4f6e0ab114b84f31ea78e07602ccf00
Abhinandan <abhi@ibeyonde.com>
2021-06-17 08:03:13 +0530

    tidying doin header and footer

--- doin/_footer.php
+++ doin/_footer.php
@@ -1,14 +1,3 @@
 </div>
-<footer class="footer">
-  <div class="container">
-    <div class="row">
-        <div class="col-md-6 col-sm-none">
-        </div>
-        <div class="col-md-6 col-sm-12 footer-social">
-        		<i class="ti-write"></i>
-        </div>
-    </div>
-  </div>
-</footer>
 </body>
 </html>
